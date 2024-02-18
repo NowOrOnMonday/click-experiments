5822a55e040f7aba7009e95bcfd72bc4a91b66ff
NowOrOnMonday <githubts@t5s5t.de>
2024-02-18 21:52:31 +0100

    Add type hints. Rename methods.

--- clicking_utilities.py
+++ clicking_utilities.py
@@ -1,7 +1,7 @@
 import pyautogui
 # from pynput.mouse import Listener, Button
 # import os
-import sys
+# import sys
 import mouse
 import time
 
@@ -12,8 +12,6 @@ clicks: list
 def on_click(mouse_event) -> None:
     global run
     global clicks
-    # if pressed:
-    # print(f"Mouse clicked at x={x}, y={y} with {button}")
     if isinstance(mouse_event, mouse.ButtonEvent):
         # print("ButtonEvent")
         if mouse_event.event_type == mouse.DOWN:
@@ -27,33 +25,23 @@ def on_click(mouse_event) -> None:
                     print(f'learned ({x},{y})')
                     clicks.append((x, y))
     elif isinstance(mouse_event, mouse.MoveEvent):
-        # print("MoveEvent")
-        # print(".", end="")
-        # print(f'click position: {pyautogui.position()}')
-        # print(f'position: {pyautogui.position()}')
-        # print(f'move position: {mouse_event.x} {mouse_event.y}')
         pass
     elif isinstance(mouse_event, mouse.WheelEvent):
-        # print("WheelEvent")
         pass
     else:
-        # print("Other event")
         pass
 
 
-def move_to_delay_click(x, y, delay=0.5) -> None:
+def move_delay_click(x: int, y: int, delay: float = 0.5) -> None:
     mouse.move(x, y)
     time.sleep(delay)
     pyautogui.click(x, y)
 
 
-
-
-
-def do_click_sequence(click_sequence: list[tuple[int, int]], delay_between_clicks: int) -> None:
-    n = len(click_sequence)
-    for x, y in click_sequence:
-        print(f'do click at {x},{y}')
+def click_coordinates(coordinates: list[tuple[int, int]], delay_between_clicks: int) -> None:
+    n = len(coordinates)
+    for x, y in coordinates:
+        print(f'click at {x},{y}')
         mouse.move(x, y)
         time.sleep(0.5)
         pyautogui.click(x, y)
@@ -66,7 +54,7 @@ def do_click_sequence(click_sequence: list[tuple[int, int]], delay_between_click
 def learn_clicks() -> list[tuple[int, int]]:
     global run
     global clicks
-    print("learning clicks ...\nFinish with click at top line.")
+    print("learning clicks (left or right click)...\nfinish learning with click in top range of screen")
     clicks = []
     mouse.hook(on_click)
     run = True
@@ -79,5 +67,5 @@ def learn_clicks() -> list[tuple[int, int]]:
 clicks = learn_clicks()
 print(f"start clicking at {clicks} in 30 seconds ...")
 time.sleep(30)
-do_click_sequence(clicks, delay_between_clicks=220)
+click_coordinates(clicks, delay_between_clicks=220)
 print("finished")
--- special_click_sequences.py
+++ special_click_sequences.py
@@ -1,26 +1,26 @@
 import time
 import sys
-from click_sequence_utilities import move_to_delay_click
+from clicking_utilities import move_delay_click
 
 
 def do_click_sequence2() -> None:
-    move_to_delay_click(1368, 109)
+    move_delay_click(1368, 109)
     time.sleep(1)
-    move_to_delay_click(696, 23)
+    move_delay_click(696, 23)
     time.sleep(1)
-    move_to_delay_click(1477, 109)
+    move_delay_click(1477, 109)
     time.sleep(1)
     sys.exit()
 
 
 def do_click_sequence_mint_fhdw() -> None:
-    move_to_delay_click(1466, 568)
+    move_delay_click(1466, 568)
     time.sleep(3)
-    move_to_delay_click(1500, 703)
+    move_delay_click(1500, 703)
     time.sleep(5)
-    move_to_delay_click(1626, 814)
+    move_delay_click(1626, 814)
     time.sleep(43)
-    move_to_delay_click(1502, 748)
+    move_delay_click(1502, 748)
     time.sleep(3)
 
 
